b54abd9a4bba45c8f26240913f80f5b0e6fd0ed8
junkeat02 <leejunk@student.usm.my>
2023-09-14 00:55:39 +0800

    reuse app.run

--- Day 69 - add user/main.py
+++ Day 69 - add user/main.py
@@ -228,3 +228,7 @@ def delete_post(post_id):
     db.session.delete(post_to_delete)
     db.session.commit()
     return redirect(url_for('get_all_posts'))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
